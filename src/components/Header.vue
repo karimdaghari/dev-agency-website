8fa3ebcbdcb774734d38b6afb3498e7a36353846
Karim Daghari <karimdxy@gmail.com>
2019-09-10 00:25:40 +0100

    Create component

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {
+  // @Prop() private propName!: propType;
+}
+</script>
+
+<style scoped lang="scss"></style>
