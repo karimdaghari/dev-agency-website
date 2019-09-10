43327513c8203caf6628fb95cbc70919bdd8bd4f
Karim Daghari <karimdxy@gmail.com>
2019-09-10 20:35:42 +0100

    Rename component appropriately

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,29 +2,29 @@
   <div class="home">
     <v-hero></v-hero>
     <v-clients />
-    <content-picture imageDirection="left">
+    <content-picture-item imageDirection="left">
       <template #title
         >bla</template
       >
-      <template #content
-        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus,
+      <template #content>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus,
         pariatur. Accusantium ut fuga dolore provident sunt laborum accusamus,
         nisi, nihil blanditiis architecto qui non dicta modi perspiciatis,
-        dolorum ullam pariatur.</template
-      >
-    </content-picture>
+        dolorum ullam pariatur.
+      </template>
+    </content-picture-item>
 
-    <content-picture imageDirection="right">
+    <content-picture-item imageDirection="right">
       <template #title
         >blou</template
       >
-      <template #content
-        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus,
+      <template #content>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus,
         pariatur. Accusantium ut fuga dolore provident sunt laborum accusamus,
         nisi, nihil blanditiis architecto qui non dicta modi perspiciatis,
-        dolorum ullam pariatur.</template
-      >
-    </content-picture>
+        dolorum ullam pariatur.
+      </template>
+    </content-picture-item>
   </div>
 </template>
 
@@ -32,13 +32,13 @@
 import { Component, Vue } from "vue-property-decorator";
 import VHero from "@/components/VHero.vue";
 import VClients from "@/components/VClients.vue";
-import ContentPicture from "@/components/ContentPicture.vue";
+import ContentPictureItem from "@/components/ContentPictureItem.vue";
 
 @Component({
   components: {
     VHero,
     VClients,
-    ContentPicture
+    ContentPictureItem
   }
 })
 export default class Home extends Vue {}
