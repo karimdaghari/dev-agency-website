9e43ae80934ce8a2ccb3a5a540a7e7a974fa2e7f
Karim Daghari <karimdxy@gmail.com>
2019-09-10 01:59:31 +0100

    Add Clients component

---
+++ src/components/Clients.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section has-background-white-ter">
+    <div class="container">
+      <h3 class="subtitle is-6 has-text-grey">
+        Trusted by the world's leading brands since 2006
+      </h3>
+      <div class="columns">
+        <div class="column">Logo</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Clients extends Vue {}
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,18 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <p>Hello</p>
+    <Clients />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import Clients from "@/components/Clients.vue";
 
-@Component
+@Component({
+  components: {
+    Clients
+  }
+})
 export default class Home extends Vue {}
 </script>
