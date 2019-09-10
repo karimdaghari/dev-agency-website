12e3bb03a56bbd24607a420c1a94cf95528171ea
Karim Daghari <karimdxy@gmail.com>
2019-09-10 20:09:44 +0100

    Add Content(with)Picture component

---
+++ src/components/ContentPicture.vue
@@ -0,0 +1,81 @@
+<template>
+  <section clas="section is-medium">
+    <div class="container-fluid">
+      <div class="columns is-variable is-4">
+        <template v-if="imageDirection === 'left'">
+          <div class="column">
+            <div class="has-background-white-ter">
+              <img
+                :src="
+                  imageSrc ||
+                    'https://x-team.com/images/pics/remote-developers.jpg'
+                "
+                alt="image here"
+                class="pull-left"
+              />
+            </div>
+          </div>
+          <div class="column is-two-fifths">
+            <div class="content has-text-left">
+              <div class="content has-text-left">
+                <h1 class="title">
+                  <slot name="title"></slot>
+                </h1>
+                <hr />
+                <p>
+                  <slot name="content"></slot>
+                </p>
+              </div>
+            </div>
+          </div>
+        </template>
+
+        <template v-else-if="imageDirection === 'right'">
+          <div class="column is-two-fifths" style="padding-left: 3%">
+            <div class="content has-text-left">
+              <h1 class="title">
+                <slot name="title"></slot>
+              </h1>
+              <hr />
+              <p>
+                <slot name="content"></slot>
+              </p>
+            </div>
+          </div>
+          <div class="column">
+            <div class="has-background-white-ter">
+              <img
+                :src="
+                  imageSrc ||
+                    'https://x-team.com/images/pics/remote-developers.jpg'
+                "
+                alt="image here"
+                class="pull-right"
+              />
+            </div>
+          </div>
+        </template>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ContentPicture extends Vue {
+  @Prop(String) readonly imageDirection!: string;
+  @Prop(String) readonly imageSrc!: string;
+}
+</script>
+
+<style scoped lang="scss">
+img.pull-left {
+  transform: translate(-15px, -10px);
+}
+
+img.pull-right {
+  transform: translate(15px, -10px);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,43 @@
   <div class="home">
     <v-hero></v-hero>
     <v-clients />
+    <content-picture imageDirection="left">
+      <template #title
+        >bla</template
+      >
+      <template #content
+        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus,
+        pariatur. Accusantium ut fuga dolore provident sunt laborum accusamus,
+        nisi, nihil blanditiis architecto qui non dicta modi perspiciatis,
+        dolorum ullam pariatur.</template
+      >
+    </content-picture>
+
+    <content-picture imageDirection="right">
+      <template #title
+        >blou</template
+      >
+      <template #content
+        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus,
+        pariatur. Accusantium ut fuga dolore provident sunt laborum accusamus,
+        nisi, nihil blanditiis architecto qui non dicta modi perspiciatis,
+        dolorum ullam pariatur.</template
+      >
+    </content-picture>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import VClients from "@/components/VClients.vue";
 import VHero from "@/components/VHero.vue";
+import VClients from "@/components/VClients.vue";
+import ContentPicture from "@/components/ContentPicture.vue";
 
 @Component({
   components: {
+    VHero,
     VClients,
-    VHero
+    ContentPicture
   }
 })
 export default class Home extends Vue {}
