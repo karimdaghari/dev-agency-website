7cb76d18ed23d24f69cc7b068f4c50b217336b6c
Karim Daghari <karimdxy@gmail.com>
2019-09-10 12:51:51 +0100

    Add clients logos

--- src/components/VClients.vue
+++ src/components/VClients.vue
@@ -1,11 +1,73 @@
 <template>
   <section class="section has-background-white-ter">
     <div class="container">
-      <h3 class="subtitle is-6 has-text-grey">
+      <h3 class="has-text-grey">
         Trusted by the world's leading brands since 2006
       </h3>
-      <div class="columns">
-        <div class="column">Logo</div>
+      <div class="level">
+        <div class="level-item">
+          <img
+            src="https://x-team.com/images/brands/fox.svg"
+            alt="Fox Logo"
+            class="image is-48x48"
+          />
+        </div>
+        <div class="level-item">
+          <img
+            src="https://x-team.com/images/brands/riot-games.svg"
+            alt="Riot games Logo"
+            class="image is-96x96"
+          />
+        </div>
+        <div class="level-item">
+          <img
+            src="https://x-team.com/images/brands/coinbase.svg"
+            alt="Coinbase Logo"
+            class="image is-96x96"
+          />
+        </div>
+        <div class="level-item">
+          <img
+            src="https://x-team.com/images/brands/threadless.svg"
+            alt="Threadless Logo"
+            class="image is-96x96"
+          />
+        </div>
+        <div class="level-item">
+          <img
+            src="https://x-team.com/images/brands/twitter.svg"
+            alt="Twitter Logo"
+            class="image is-48x48"
+          />
+        </div>
+        <div class="level-item">
+          <img
+            src="https://x-team.com/images/brands/dell.svg"
+            alt="Dell Logo"
+            class="image is-48x48"
+          />
+        </div>
+        <div class="level-item">
+          <img
+            src="https://x-team.com/images/brands/discovery.svg"
+            alt="Discovery Logo"
+            class="image is-96x96"
+          />
+        </div>
+        <div class="level-item">
+          <img
+            src="https://x-team.com/images/brands/kaplan.svg"
+            alt="Kaplan Logo"
+            class="image is-96x96"
+          />
+        </div>
+        <div class="level-item">
+          <img
+            src="https://x-team.com/images/brands/beachbody.svg"
+            alt="Beachbody Logo"
+            class="image is-128x128"
+          />
+        </div>
       </div>
     </div>
   </section>
